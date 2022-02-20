b04954ea4b6a194788cce3b6bd2bc3960b0ecf07
Tang <1648375651@qq.com>
2022-02-20 13:12:43 +0800

    用户模块

---
+++ src/main/java/com/haoshuai/intelligentcommunity/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.haoshuai.intelligentcommunity.service.impl;
+
+import com.haoshuai.intelligentcommunity.entity.User;
+import com.haoshuai.intelligentcommunity.mapper.UserMapper;
+import com.haoshuai.intelligentcommunity.service.IUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author TangHaoShuai
+ * @since 2022-02-20
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+}
